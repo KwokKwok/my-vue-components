ced3e99b3256df04cb0d95b11e7daaad7901d7a4
郭垒 <kwokglory@outlook.com>
2018-12-20 09:40:41 +0800

    init

---
+++ src/components/UnderlineLabel.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="underline-label">
+    <label>
+      <slot></slot>
+    </label>
+    <div class="line"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Prop, Component } from "vue-property-decorator";
+@Component
+export default class UnderlineLabel extends Vue {}
+</script>
+
+<style lang="scss">
+.underline-label {
+  width: auto;
+  display: inline-block;
+
+  label {
+    transition: 0.3s all ease-in-out;
+  }
+
+  .line {
+    margin-top: 4px;
+    background-color: blue;
+    width: 0%;
+    margin: 0 auto;
+    height: 2px;
+    transition: 0.3s all ease-in-out;
+  }
+
+  &:hover {
+    label {
+      color: blue;
+    }
+    .line {
+      width: 100%;
+    }
+  }
+}
+</style>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <underline-label>你好</underline-label>
+    <button @click="go">测试</button>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import UnderlineLabel from "@/components/UnderlineLabel.vue"; // @ is an alias to /src
 
 @Component({
   components: {
     HelloWorld,
-  },
+    UnderlineLabel
+  }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  isActive: boolean = false;
+  go() {
+    this.isActive = !this.isActive;
+  }
+}
 </script>
+
+<style lang="scss" scoped>
+.underline-label {
+  margin: 10px;
+}
+</style>
+
